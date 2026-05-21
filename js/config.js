10ce7c62126739d9ed4f14da08c1324f42785433
imprjoseph <125981302+imprjoseph@users.noreply.github.com>
2026-05-22 06:58:40 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
  */
 const CONFIG = {
   // ★ 部署 GAS Web App 後，將 URL 填入此處
-  GAS_WEB_APP_URL: '請填入 Google Apps Script Web App URL',
+  GAS_WEB_APP_URL: 'AKfycbxuUu9GwsYI2jXI5e4fuN-bs3asJNyrliXVVA6rNCJ-mCJ1mLNn4j_hjXIqrkM2VclF',
 
   SYSTEM_NAME: '派遣人員勤務管理系統',
   SYSTEM_SHORT: 'imPR 派遣系統',
